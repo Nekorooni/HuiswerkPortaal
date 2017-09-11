863762f9e680beb68fb00d094b0a458b99860a50
rooni <nekorooni@gmail.com>
2017-09-11 14:34:02 +0200

    Fix up register page
    So bootstrap works

--- pages/register.php
+++ pages/register.php
@@ -6,6 +6,7 @@ if(isset($_POST['username'])) {
     $inputPass = htmlspecialchars($_POST['password']);
     $inputPas2 = htmlspecialchars($_POST['repeat']);
     $inputMail = htmlspecialchars($_POST['email']);
+    $klas = htmlspecialchars($_POST['klas']);
 
     //Validate input
     $errors = [];
@@ -24,7 +25,7 @@ if(isset($_POST['username'])) {
       }
     } else {
       //Register user
-      $user = User::register($inputUser, $inputMail, $inputPass, 'user');
+      $user = Leerling::register($inputUser, $inputMail, $inputPass, $klas, 'leerling');
       if($user) {
           echo $user->getUsername();
 
@@ -37,18 +38,30 @@ if(isset($_POST['username'])) {
 }
 ?>
 <div class="container">
-    <h1>
-        REGISTER
-    </h1>
-
-    <form action="?page=register" method="POST">
-
-        <input type="text" placeholder="Username" required name="username"/><br/>
-        <input type="email" placeholder="E-mail" required name="email"/><br/>
-        <input type="password" placeholder="Password" required name="password"/><br/>
-        <input type="password" placeholder="Repeat" required name="repeat"/><br/>
-
-        <input type="submit" value="Register"/>
-    </form>
-
+    <div class="row"><h3>Registeren</h3></div>
+    <div class="row">
+        <form action="?page=login" method="POST">
+            <div class="form-group">
+                <label for="username">Leerling nr.</label>
+                <input type="text" class="form-control" id="username" placeholder="Nummer" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" placeholder="Email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="klas">Klas</label>
+                <input type="text" class="form-control" id="klas" placeholder="Klas" name="klas" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="repeat">Repeat</label>
+                <input type="password" class="form-control" id="repeat" placeholder="Password" name="repeat" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
 </div>
